e48d96d64561ea0ae9e3eb872d84488aad33f0ad
your-user-name <your-email-address-on-gitee>
2023-06-16 17:41:28 +0800

    修改信息管理

--- api/models/tenant.py
+++ api/models/tenant.py
@@ -7,7 +7,7 @@ from datetime import date
 
 class Tenant(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=150, blank=True)
     real_name = models.CharField(max_length=150, blank=True) #法人名称
     company = models.CharField(max_length=150, blank=True) #公司名称
     contactName = models.CharField(max_length=150, blank=True)  # 联系人名称
--- api/views/messageManage.py
+++ api/views/messageManage.py
@@ -8,11 +8,10 @@ from api.models import Tenant
 def add_tenant(request):
     if request.method == 'POST':
         # 从请求中获取客户信息
-        data = json.loads(request.body)
-        real_name = data['real_name']
-        company = data['company']
-        contactName = data['contactName']
-        contactNumber = data['contactNumber']
+        real_name = request.GET.get('real_name')
+        company = request.GET.get('company')
+        contactName = request.GET.get('contactName')
+        contactNumber = request.GET.get('contactNumber')
         # 创建新的客户对象并保存到数据库中
         tenant = Tenant(real_name=real_name, company=company,
                         contactName=contactName,
@@ -32,16 +31,15 @@ def delete_tenant(request, tenant_id):
     return HttpResponse('Tenant deleted successfully!')
 
 
-def update_tenant(request, tenant_id):
+def update_tenant(request, company):
     if request.method == 'POST':
         # 根据客户id获取客户对象
-        tenant = Tenant.objects.get(id=tenant_id)
-        data = json.loads(request.body)
+        tenant = Tenant.objects.get(company=company)
         # 更新客户信息
-        tenant.real_name = data['real_name']
-        tenant.company = data['company']
-        tenant.contactName = data['contactName']
-        tenant.contactNumber = data['contactNumber']
+        tenant.real_name = request.GET.get('real_name')
+        tenant.company = request.GET.get('company')
+        tenant.contactName = request.GET.get('contactName')
+        tenant.contactNumber = request.GET.get('contactNumber')
 
         # 保存客户对象到数据库中
         tenant.save()
@@ -50,6 +48,6 @@ def update_tenant(request, tenant_id):
         return HttpResponse('tenant updated successfully!')
 
 
-def view_tenant(request, tenant_id):
-    tenant = Tenant.objects.get(id=tenant_id)
+def view_tenant(company):
+    tenant = Tenant.objects.get(company=company)
     return tenant
