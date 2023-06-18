b7ce35f437543e4b837b7e621fd9c18f0466c997
KaiBa Fou <foueducation@gmail.com>
2023-06-18 15:55:31 +0700

    [config]: config database

--- backend/settings.py
+++ backend/settings.py
@@ -87,6 +87,14 @@ AUTH_USER_MODEL = 'api.CustomUser'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
