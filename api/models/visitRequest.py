b4a8a1938c4e1defea0012eabb07ae416ef68714
Coralnws <ngwengshan025@hotmail.com>
2023-06-20 08:21:05 +0800

    [alter]:alter visit request model

--- api/models/visitRequest.py
+++ api/models/visitRequest.py
@@ -6,9 +6,9 @@ from api.models.house import House
 class VisitRequest(models.Model):
     name = models.CharField(max_length=100)
     ic = models.CharField(max_length=20)
-    visitTime = models.DateTimeField()
+    visit_time = models.DateTimeField()
     inviter = models.ForeignKey(House, related_name='inviter',on_delete=models.CASCADE,null=True,blank=True)
-    contactNumber = models.CharField(max_length=20)
+    contact_number = models.CharField(max_length=20)
     otp = models.CharField(max_length=8)
     otp_sent = models.IntegerField()
     house = models.ForeignKey(House, related_name='invite_house',on_delete=models.CASCADE,null=True,blank=True)
