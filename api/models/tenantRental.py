91c79168b873a53ab974375dd08d6da849fdb303
Coralnws <ngwengshan025@hotmail.com>
2023-06-20 16:18:15 +0800

    [add]:add model

--- api/models/tenantRental.py
+++ api/models/tenantRental.py
@@ -1,12 +1,13 @@
 import uuid
-from django.db import models
 from django.utils import timezone
+import datetime
+from django.db import models
+from datetime import date
 
-
-class RentalInfo(models.Model):
+class TenantRental(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     house = models.ForeignKey("House", on_delete=models.CASCADE, null=False, blank=False)
-    tenant = models.ForeignKey("Tenant", on_delete=models.CASCADE, null=False, blank=False)
+    rental = models.ForeignKey("RentalInfo", on_delete=models.CASCADE, null=False, blank=False)
 
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
