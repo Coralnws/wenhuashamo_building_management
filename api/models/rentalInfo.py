04498b197cc61156ac7b8a1a37a27209b8304442
Coralnws <ngwengshan025@hotmail.com>
2023-06-20 16:25:09 +0800

    [add]:add tenantRental model

--- api/models/rentalInfo.py
+++ api/models/rentalInfo.py
@@ -7,6 +7,17 @@ class RentalInfo(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     house = models.ForeignKey("House", on_delete=models.CASCADE, null=False, blank=False)
     tenant = models.ForeignKey("Tenant", on_delete=models.CASCADE, null=False, blank=False)
+    createdTime = models.DateTimeField(default=timezone.now)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    rental_fee = models.IntegerField(default=0)  #租赁费
+    management_fee = models.IntegerField(default=0)  #物业费
+    nextRentalDeadline = models.DateTimeField(null=True)   #租赁费截止 每次付款更新截止日期，若截止日期 < 现在则欠费
+    nextManagementFeeDeadline = models.DateTimeField(null=True)   #物业费截止
+    paidRentalDate = models.DateTimeField(null=True)
+    paidManagementDate = models.DateTimeField(null=True)
+    ispaid_rental = models.BooleanField(default=False)
+    ispaid_management = models.BooleanField(default=False)
 
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(default=timezone.now)
+    createdAt = models.DateTimeField(default=timezone.now)
+    updatedAt = models.DateTimeField(default=timezone.now)
--- api/models/tenantRental.py
+++ api/models/tenantRental.py
@@ -6,8 +6,8 @@ from datetime import date
 
 class TenantRental(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    house = models.ForeignKey("House", on_delete=models.CASCADE, null=False, blank=False)
-    rental = models.ForeignKey("RentalInfo", on_delete=models.CASCADE, null=False, blank=False)
+    house = models.ForeignKey("House",on_delete=models.CASCADE, null=False, blank=False)
+    rental = models.ForeignKey("RentalInfo",on_delete=models.CASCADE, null=False, blank=False)
 
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
