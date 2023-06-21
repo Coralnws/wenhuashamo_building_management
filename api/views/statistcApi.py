13866650df86c26408825a966118bd74bc64b308
Coralnws <ngwengshan025@hotmail.com>
2023-06-21 10:33:02 +0800

    [add]:add statisticApi.py

---
+++ api/views/statistcApi.py
@@ -0,0 +1,17 @@
+
+from datetime import timedelta
+from datetime import datetime,timedelta
+import time
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.utils import timezone
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+from api.models import *
+from ..utils import *
+from django.forms.models import model_to_dict
+from django.db.models import Q
+import operator
+from api.utils import *
+from api.error_utils import *
+
